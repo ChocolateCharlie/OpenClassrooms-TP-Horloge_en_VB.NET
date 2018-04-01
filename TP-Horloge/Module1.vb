15d28270dbf389eb271c7df078f761b99abe3fff
ChocolateCharlie <aurore.amrit@gmail.com>
2018-04-01 19:35:51 +0200

    Finir le mini-TP

--- TP-Horloge/Module1.vb
+++ TP-Horloge/Module1.vb
@@ -2,6 +2,14 @@
 
     Sub Main()
 
+        ' ATTENTION Boucle infinie !
+        While True
+            ' Console.WriteLine("~.~.~ HORLOGE ~.~.~")
+            Console.WriteLine("--- " & Date.Now.ToLongTimeString & " ---")
+            System.Threading.Thread.Sleep(1000)
+            Console.Clear()
+        End While
+
     End Sub
 
 End Module
